3798bcb0c7e33a18869e9b500f653a0e078614b1
Jack <JPAnderson313@gmail.com>
2016-03-01 23:39:46 -0600

    Final Project. This Website can convert to and from morse code.

--- project_1_javascript.js
+++ project_1_javascript.js
@@ -2,40 +2,37 @@
 
 
 function start(){
-    console.log("1");
-    var radioButton = document.getElementsByName("choice");
-    console.log("2");
-    var checked;
-    for(var i = 0; i < radioButton.length; i++){
-        console.log("3");
-        if(radioButton[i].checked){
-            if(radioButton[i].value == "Encrypt"){
-                var enc = encrypt();
-            } else if(radioButton[i].value == "Decrypt"){
-                var dec = decrypt();
+    var radioButton = document.getElementsByName("choice"); //get the radio buttons and put them ina lit
+    for(var i = 0; i < radioButton.length; i++){ //For every item in the list
+        if(radioButton[i].checked) //See if the R.B. is checked
+            if(radioButton[i].value == "Encrypt"){ //if yes, if the R.Bs value = "Encrypt"
+                var enc = encrypt(); //run the encrypt function
+            } else if(radioButton[i].value == "Decrypt"){ //If it's anything else
+                var dec = decrypt(); //run decrypt
             }
         }
     }
 
     function encrypt(){
-        var textToChange = document.getElementById("userTextArea").value;
-        var userTextArray = textToChange.split("");
-        console.log(userTextArray[1]); <!--GOOD-->
-        var morseArray = [];
+        var textToChange = document.getElementById("userTextArea").value; //Get the text in the user box
+        var userTextArray = textToChange.split(""); //Put each CHARACTER into an array
+        var morseArray = []; //Make an empty array. We'll populate this with morse code later
 
-        for(var i = 0; i < userTextArray.length; i++){
-            var stage = userTextArray[i].toUpperCase();
-            var addToMorse = convert(stage);
-            morseArray.push(addToMorse);
+        for(var i = 0; i < userTextArray.length; i++){ //For each element
+            var stage = userTextArray[i].toUpperCase(); //Convert it to upper case (for consistency and error handling)
+            var addToMorse = convert(stage); //convert it to MORSE
+            morseArray.push(addToMorse); //push it into our empty array
         }
 
-        var outputText = morseArray.join(" ");
-        var polishedOutputText = outputText.replace(",", " ");
+        //When it's done...
+        var outputText = morseArray.join(" "); //Join the array elements. Separate with a space
+        var polishedOutputText = outputText.replace(",", " "); //replace commas added by program with spaces
         var outPutBox = document.getElementById("outputTextArea");
-        outPutBox.value = polishedOutputText;
+        outPutBox.value = polishedOutputText; //Put the finalized string into the box
     }
 
     function decrypt(){
+        //decrypt is fundamentally similar to encrypt. Basically just works in reverse
         var textToChange2 = document.getElementById("userTextArea").value;
         var userTextArray2 = textToChange2.split(" ");
         var letterArray = [];
@@ -46,198 +43,327 @@ function start(){
             letterArray.push(addToLetters);
         }
 
-        var outputText2 = letterArray.join("");
-        var polishedOutputText2 = outputText2.replace(",", " ");
+        var outputText2 = letterArray.join(""); //Join with a comma (default) instead of a space
+        var polishedOutputText2 = outputText2.replace(",", " "); //...then replace the commas with spaces.
         var outputBox2 = document.getElementById("outputTextArea");
         outputBox2.value = polishedOutputText2;
     }
 
-    function convert2(morse){
+    //This function converts morse to English
+    function convert2(morse){ //<--sent from ^
         var letter;
 
-        switch(morse){
+        switch(morse){ //convert the morse character to it's letter equivalent and assign it to letter
             case ".-":
                 letter = "A";
-                break
+                break;
             case "-...":
                 letter = "B";
-                break
+                break;
             case "-.-.":
                 letter = "C";
-                break
+                break;
             case "-..":
                 letter = "D";
-                break
+                break;
             case ".":
                 letter = "E";
-                break
+                break;
             case "..-.":
                 letter = "F";
-                break
+                break;
             case "--.":
                 letter = "G";
-                break
+                break;
             case "....":
                 letter = "H";
-                break
+                break;
             case "..":
                 letter = "I";
-                break
+                break;
             case ".---":
                 letter = "J";
-                break
+                break;
             case "-.-":
                 letter = "K";
-                break
+                break;
             case ".-..":
                 letter = "L";
-                break
+                break;
             case "--":
                 letter = "M";
-                break
+                break;
             case "-.":
                 letter = "N";
-                break
+                break;
             case "---":
                 letter = "O";
-                break
+                break;
             case ".--.":
                 letter = "P";
-                break
+                break;
             case "--.-":
                 letter = "Q";
-                break
+                break;
             case ".-.":
                 letter = "R";
-                break
+                break;
             case "...":
                 letter = "S";
-                break
+                break;
             case "-":
                 letter = "T";
-                break
+                break;
             case "..-":
                 letter = "U";
-                break
+                break;
             case "...-":
                 letter = "V";
-                break
+                break;
             case ".--":
                 letter = "W";
-                break
+                break;
             case "-..-":
                 letter = "X";
-                break
+                break;
             case "-.--":
                 letter = "Y";
-                break
+                break;
             case "--..":
                 letter = "Z";
-                break
-            case "/":
-                letter = " ";
-            default:
+                break;
+            case "-----":
+                letter = "0";
+                break;
+            case ".----":
+                letter = "1";
+                break;
+            case "..---":
+                letter = "2";
+                break;
+            case "...--":
+                letter = "3";
+                break;
+            case "....-":
+                letter = "4";
+                break;
+            case ".....":
+                letter = "5";
+                break;
+            case "-....":
+                letter = "6";
+                break;
+            case "--...":
+                letter = "7";
+                break;
+            case "---..":
+                letter = "8";
+                break;
+            case "----.":
+                letter = "9";
+                break;
+            case ".-.-.":
+                letter = "+";
+                break;
+            case "-...-":
+                letter = "=";
+                break;
+            case "-..-.":
+                letter = "/";
+                break;
+            case "..--..":
+                letter = "?";
+                break;
+            case "..--.-":
+                letter = "_";
+                break;
+            case ".-..-.":
+                letter = '"';
+                break;
+            case ".-.-.-":
+                letter = ".";
+                break;
+            case ".--.-.":
+                letter = "@";
+                break;
+            case ".----.":
+                letter = "'";
+                break;
+            case "-....-":
                 letter = "-";
+                break;
+            case "-.-.-.":
+                letter = ";";
+                break;
+            case "-.-.--":
+                letter = "!";
+                break;
+            case "--..--":
+                letter = ",";
+                break;
+            case "---...":
+                letter = ":";
+                break;
+
+            default:
+                letter = " ";
         }
         return letter;
     }
 
 
-    function convert(letter){
+    //This function converts english to morse
+    function convert(letter){ // <-- sent from ^
         var morse;
 
-        switch(letter){
+        switch(letter){ //convert the letter to it's morse equivalent and assign it to morse
             case "A":
                 morse = ".-";
-                break
+                break;
             case "B":
                 morse = "-...";
-                break
+                break;
             case "C":
                 morse = "-.-.";
-                break
+                break;
             case "D":
                 morse = "-..";
-                break
+                break;
             case "E":
                 morse = ".";
-                break
+                break;
             case "F":
                 morse = "..-.";
                 break;
             case "G":
                 morse = "--.";
-                break
+                break;
             case "H":
                 morse = "....";
-                break
+                break;
             case "I":
                 morse = "..";
-                break
+                break;
             case "J":
                 morse = ".---";
-                break
+                break;
             case "K":
                 morse = "-.-";
-                break
+                break;
             case "L":
                 morse = ".-..";
-                break
+                break;
             case "M":
                 morse = "--";
-                break
+                break;
             case "N":
                 morse = "-.";
-                break
+                break;
             case "O":
                 morse = "---";
-                break
+                break;
             case "P":
                 morse = ".--.";
-                break
+                break;
             case "Q":
                 morse = "--.-";
-                break
+                break;
             case "R":
                 morse = ".-.";
-                break
+                break;
             case "S":
                 morse = "...";
-                break
+                break;
             case "T":
                 morse = "-";
-                break
+                break;
             case "U":
                 morse = "..-";
-                break
+                break;
             case "V":
                 morse = "...-";
-                break
+                break;
             case "W":
                 morse = ".--";
-                break
+                break;
             case "X":
                 morse = "-..-";
-                break
+                break;
             case "Y":
                 morse = "-.--";
-                break
+                break;
             case "Z":
                 morse = "--..";
-                break
+                break;
             case " ":
                 morse = "/";
-                break
+                break;
             case ".":
-                morse = "";
+                morse = ".-.-.-";
+                break;
             case ",":
-                morse = "";
+                morse = "--..--";
+                break;
             case "'":
-                morse = "";
+                morse = ".----.";
+                break;
+            case "?":
+                morse = "..--..";
+                break;
+            case "_":
+                morse = "..--.-";
+                break;
+            case '"':
+                morse = ".-..-.";
+                break;
+            case "@":
+                morse = ".--.-.";
+                break;
+            case "-":
+                morse = "-....-";
+                break;
+            case ";":
+                morse = "-.-.-.";
+                break;
+            case "!":
+                morse = "-.-.--";
+                break;
+            case ":":
+                morse = "---...";
+                break;
+            case "0":
+                morse = "-----";
+                break;
+            case "9":
+                morse = "----.";
+                break;
+            case "8":
+                morse = "---..";
+                break;
+            case "7":
+                morse = "--...";
+                break;
+            case "6":
+                morse = "-....";
+                break;
+            case "5":
+                morse = ".....";
+                break;
+            case "4":
+                morse = "....-";
+                break;
+            case "3":
+                morse = "...--";
+                break;
+            case "2":
+                morse = "..---";
+                break;
+            case "1":
+                morse = ".----";
+                break;
             default:
                 morse = " ";
         }
         return morse;
     }
 
-}
